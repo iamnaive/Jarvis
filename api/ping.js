d51a438b013681f750c42e284a7bae8a02d848c3
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-18 10:28:30 +1000

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,5 @@
+export const runtime = 'edge';
+export default async function handler(req) {
+  if (req.method === 'GET') return new Response('pong', { status: 200 });
+  return new Response('ok', { status: 200 });
+}
